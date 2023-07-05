247cfa22425fce537e65c58ccc8919b4651cd7d5
keliankaz <kdascher@ucsc.edu>
2023-07-05 12:09:36 -0700

    add a+

--- stat_utils.py
+++ stat_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Optional
 
 
 def jitter(x, sigma):
@@ -62,3 +63,67 @@ def bootstrap_df(df, f, kwarg=None, boot=1000):
     return np.array(
         [f(df.sample(n=len(df), replace=True), **kwarg) for _ in np.arange(boot)]
     )
+
+
+def get_apositive(
+    mag: np.ndarray, 
+    t: np.ndarray, 
+    dMc: float = 0.2, 
+    Mref: float = 5, 
+    b_pos: Optional[float] = None,
+):
+    """Returns the a(+) values for a given set of magnitudes M and 
+    times t using b positive.
+    
+    If b_pos is not provided, it is calculated from the data using the dMc
+    value provided.
+    
+    Input:
+        mag: array of magnitudes
+        t: array of times
+        dMc: magnitude of completeness
+        Mref: reference magnitude
+        b_pos: b-value for positive magnitudes
+        
+    Output:
+        r_pos: pairwise estimates of the rate of Mref earthquakes (r_pos = 1/dt_pos)
+        dt_pos: pairwise interevent times, corrected for dMc but not scaled 
+            to Mref
+        t_measure: absolute time of the larger earthquake with M > mag + dMc.
+            (Usefule for associating r_pos with the mean time of the pair.)
+    
+    """
+    
+    dt_pos = []
+    t_measure = []
+    
+    assert not np.any(np.diff(t) < 0), "t must be sorted in increasing order."
+    
+    for mag_i, t_i in zip(mag,t):
+        next_larger_event_bool= (
+            (mag >= mag_i + dMc) &
+            (t > t_i)
+        )
+        
+        if np.any(next_larger_event_bool):
+            # argmax returns the first True value in the array of next_larger_event_bool
+            next_larger_index = np.argmax(next_larger_event_bool)
+            dt_pos.append(t[next_larger_index] - t_i)
+            t_measure.append(t[next_larger_index])
+        else:
+            dt_pos.append(np.nan)
+            t_measure.append(np.nan)
+            
+    dt_pos = np.array(dt_pos)
+    t_measure = np.array(t_measure)
+            
+    if b_pos is None:
+        b_pos = get_bpositive(mag, dMc)
+    
+    dt_pos_unscaled = dt_pos * 10 ** (-b_pos * dMc)
+    dt_pos = dt_pos_unscaled * 10 ** (-b_pos * (mag + dMc - Mref))
+    
+    r_pos = 1 / dt_pos
+    
+    return r_pos, dt_pos, t_measure
+        
